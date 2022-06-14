224f5f24919a072bbac38be2ac1b0e595e587060
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-06-15 00:28:47 +0700

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val time = 20
+    if (time > 10){
+        println("good evening")
+    }
+    else if (time < 10)
+        println("good morning")
+    else{
+        println("not today")
+    }
+}
